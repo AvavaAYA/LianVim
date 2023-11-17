42a728119a7cbd5b3aebb606de9820a742c2431d
AvavaAYA <nana7777777mi@gmail.com>
2023-11-17 22:30:26 +0800

    [UPDATE]

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,17 @@ opt.shiftwidth = 4
 opt.tabstop = 4
 opt.smartindent = true -- Insert indents automatically
 opt.mouse = "n" -- Enable mouse mode
+
+opt.clipboard = "unnamedplus"
+opt.wrap = true
+opt.clipboard = ""
+
+opt.spelllang = "en,cjk"
+-- vim.opt.spell = true
+opt.spelloptions = "camel"
+opt.scrolloff = 5
+opt.indentexpr = ""
+opt.foldmethod = "indent"
+opt.foldlevel = 99
+opt.foldenable = true
+opt.foldlevelstart = 99
--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,18 +1,21 @@
 return {
-    { "sbdchd/neoformat" },
-    {
-        "stevearc/conform.nvim",
-        opts = {
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "isort", "black" },
-                javascript = { { "prettierd", "prettier" } },
-                markdown = { { "prettierd", "prettier" } },
-                c = { "astyle" },
-                cpp = { "astyle" },
-                rust = { "rustfmt" },
-                nix = { "alejandra" },
-            },
-        },
+  { "sbdchd/neoformat" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { { "prettierd", "prettier" } },
+        markdown = { { "prettierd", "prettier" } },
+        c = { "clang-format", "astyle" },
+        cpp = { "clangformat", "astyle" },
+        rust = { "rustfmt" },
+        nix = { "nixfmt" },
+      },
     },
+  },
+  {
+    "LhKipp/nvim-nu",
+  },
 }
